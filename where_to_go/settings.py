808dc2907f8bb1831d48fdbfd94ff5fe15889627
veffhz <veffhz@example.com>
2020-10-20 14:41:50 +0300

    Init django app

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -16,6 +16,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'posterapp',
 ]
 
 MIDDLEWARE = [
@@ -30,10 +32,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'where_to_go.urls'
 
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
