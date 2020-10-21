615adb4c4a5e556fdc5da1821d2a9a44a67182fe
veffhz <veffhz@example.com>
2020-10-21 16:09:30 +0300

    Fixed settings.py

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -95,7 +95,9 @@ USE_TZ = True
 
 STATIC_URL = os.getenv('STATIC_URL', '/static/')
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [STATIC_DIR]
 
 MEDIA_ROOT = os.getenv("MEDIA_ROOT", os.path.join(BASE_DIR, 'media'))
 
