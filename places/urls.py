f9a5f02803cff8a2ae7d9e4d1ae773e5ada6f5f3
veffhz <veffhz@example.com>
2020-10-21 12:21:16 +0300

    Rename places path

--- places/urls.py
+++ places/urls.py
@@ -4,5 +4,5 @@ from places.views import MainView, PlaceDetailView
 
 urlpatterns = [
     path('', MainView.as_view(), name='main'),
-    path('place/<int:place_id>', PlaceDetailView.as_view(), name='place-detail'),
+    path('places/<int:place_id>', PlaceDetailView.as_view(), name='place-detail'),
 ]
